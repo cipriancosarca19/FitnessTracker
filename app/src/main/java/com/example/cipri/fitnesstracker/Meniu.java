82704ec603007fe300ef457ed349da5ec1406dd9
Petruta Deac <deacpetruta@gmai.com>
2016-11-28 15:18:56 +0200

    new feature

--- app/src/main/java/com/example/cipri/fitnesstracker/Meniu.java
+++ app/src/main/java/com/example/cipri/fitnesstracker/Meniu.java
@@ -1,13 +1,35 @@
 package com.example.cipri.fitnesstracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Meniu extends AppCompatActivity {
 
+    Button despre;
+    Button masa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meniu);
+
+        despre=(Button)findViewById(R.id.btnDespre);
+        despre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(v.getContext(),Despre.class);
+                startActivity(intent);
+            }
+        });
+        masa=(Button)findViewById(R.id.btnMasa);
+        masa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(v.getContext(),Masa.class);
+                startActivity(intent);
+            }
+        });
     }
 }
