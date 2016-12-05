89668bbf3f4517142e193365b4d3932d8bbe7d2f
Petruta Deac <deacpetruta@gmai.com>
2016-12-05 19:59:40 +0200

    despre

--- app/src/main/java/com/example/cipri/fitnesstracker/alimente.java
+++ app/src/main/java/com/example/cipri/fitnesstracker/alimente.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class Alimente extends AppCompatActivity {
 
-    public static List<Aliment> alimente=new ArrayList<>();
+    public static List<Aliment> alimente;
     ListView lv;
     Context context;
 
@@ -20,14 +20,23 @@ public class Alimente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alimente);
+
+
+        alimente=new ArrayList<Aliment>();
         alimente.add(new Aliment("Banana",200,200));
         alimente.add(new Aliment("Apple",200,200));
         alimente.add(new Aliment("Berry",200,200));
 
-
-        lv=(ListView)findViewById(R.id.lista_alimente);
+        lv=(ListView)findViewById(R.id.listaAlimente);
+        List<String> denumiri=new ArrayList<String>();
+        List<Integer> calorii=new ArrayList<Integer>();
+        for (Aliment a:alimente)
+        {
+            denumiri.add(a.getDenumire().toString());
+            calorii.add(a.getCalorii());
+        }
         context=this;
-        lv.setAdapter(new AdapterAlimente(this,alimente));
+        lv.setAdapter(new AdapterAlimente(this,denumiri,calorii));
 
 
 
