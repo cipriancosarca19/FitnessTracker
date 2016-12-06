5c21941ed85e57cda48c51010a78a9074ef67bf3
Petruta Deac <deacpetruta@gmai.com>
2016-12-06 14:49:15 +0200

    login and custom list alimente

--- app/src/main/java/com/example/cipri/fitnesstracker/ContNou.java
+++ app/src/main/java/com/example/cipri/fitnesstracker/ContNou.java
@@ -2,6 +2,13 @@ package com.example.cipri.fitnesstracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.cipri.fitnesstracker.classes.Gen;
+import com.example.cipri.fitnesstracker.classes.Utilizator;
 
 public class ContNou extends AppCompatActivity {
 
@@ -9,5 +16,30 @@ public class ContNou extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cont_nou);
+
+        Button btnTrimite=(Button)findViewById(R.id.btnTrimite);
+        btnTrimite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText etNume=(EditText)findViewById(R.id.et_nume);
+                EditText etPrenume=(EditText)findViewById(R.id.et_prenume);
+                EditText etEmail=(EditText)findViewById(R.id.et_email);
+                EditText etInaltime=(EditText)findViewById(R.id.et_inaltime);
+                EditText etGreutate=(EditText)findViewById(R.id.et_greutate);
+                EditText etUtilizator=(EditText)findViewById(R.id.et_nickname);
+                EditText etParola=(EditText)findViewById(R.id.etParola);
+
+
+                Utilizator utilizatorNou=new Utilizator(etNume.getText().toString(),etPrenume.getText().toString(),etEmail.getText().toString(), etParola.getText().toString(), etGreutate.getText().toString(), Gen.FEMININ, etInaltime.getText().toString(),etUtilizator.getText().toString());
+                MainActivity.utilizatori.add(utilizatorNou);
+
+
+                    Toast.makeText(getApplicationContext(),"Utilizator inregistrat cu succes!",Toast.LENGTH_SHORT).show();
+
+
+
+            }
+        });
     }
 }
